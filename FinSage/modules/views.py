48d00aca46997aeee971d6a439d3c0bb102be8b2
Kartik Sood <kartiksood2002@gmail.com>
2023-08-11 01:00:25 +0530

    views changes

--- FinSage/modules/views.py
+++ FinSage/modules/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import Modules
 
 def index(request):
+    data = Modules.objects.all().values()
+    context = {
+        #'form': forms[cipher_choice](),
+        'data': data
+        
+    }
+    
+
+    #print(data)
     return render(request,'modules/index.html',{'sidebar':True})
 
 def modules(request):
