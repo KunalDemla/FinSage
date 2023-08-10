c03e62f5d43a2859645a804ffc6236f4cf95dc5e
Kartik Sood <kartiksood2002@gmail.com>
2023-08-11 00:28:28 +0530

    modules func in view

--- FinSage/modules/views.py
+++ FinSage/modules/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'modules/index.html',{'sidebar':True})
+
+def modules(request):
+    return render(request,'modules/index.html',{'sidebar':True})
