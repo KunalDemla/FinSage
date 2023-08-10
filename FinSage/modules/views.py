8a4984a3101d2a0e1b0ef55778704ec9cffaded2
Kartik Sood <kartiksood2002@gmail.com>
2023-08-11 00:30:37 +0530

    reco func in view

--- FinSage/modules/views.py
+++ FinSage/modules/views.py
@@ -6,3 +6,7 @@ def index(request):
 
 def modules(request):
     return render(request,'modules/index.html',{'sidebar':True})
+
+def recommendations(request):
+    return render(request,'modules/index.html',{'sidebar':True})
+
